60a1481e64f825545cfd97aaa3abf80d46c9a340
Zoey Hamm <339622581@GoTVDSB.ca>
2025-02-18 13:33:25 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Topic_5___If_Statements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
