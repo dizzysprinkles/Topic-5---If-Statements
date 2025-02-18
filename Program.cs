c7864b81e4631e05777615f38df2329ecd8f8b09
Zoey Hamm <339622581@GoTVDSB.ca>
2025-02-18 13:52:19 -0500

    Started assignment

--- Program.cs
+++ Program.cs
@@ -4,43 +4,19 @@
     {
         static void Main(string[] args)
         {
-            //Print Good Job if variable grade is higher/equal than 80
-            int grade, radius, number;
+            int personOne, personTwo, personThree;
 
-            number = 7;
+            // Read in the three ages
 
-            if (number >= 5 && number <= 15)
-            {
-                Console.WriteLine("This code actually worked!");
-            }
+            //Print messages for when...
 
-
-            //Console.WriteLine("Hey there! What is your current grade average (whole numbers) ?");
-            //Int32.TryParse(Console.ReadLine(), out grade);
-
-            //if (grade >= 80)
-            //{
-            //    Console.WriteLine("Good Job! That is a great grade average to have!");
-            //}
-            //else
-            //{
-            //    Console.WriteLine("Study harder!");
-            //}
-
-            //Console.WriteLine("Give me a radius");
-            //Int32.TryParse(Console.ReadLine(), out radius);
-            //if (radius <0)
-            //{
-            //    Console.WriteLine("Negative radii are illegal!");
-            //}
-            //else if (radius == 0)
-            //{
-            //    Console.WriteLine("You have a dot.");
-            //}
-            //else
-            //{
-            //    Console.WriteLine("The diameter is " + radius*2 + ".");
-            //}
+            // 1 > 2
+            // 3 < 1
+            // 2 == 3
+            // 2 < 3 , 1
+            // 1 > 2,3
+            // 2 == 1 or 2 == 3  but not both
+            
             
         }
     }
