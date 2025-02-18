973423643f759fcb8033a14d6f153a28ab88facb
Zoey Hamm <339622581@GoTVDSB.ca>
2025-02-18 13:49:44 -0500

    Finished Tutorial

--- Program.cs
+++ Program.cs
@@ -4,7 +4,44 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Print Good Job if variable grade is higher/equal than 80
+            int grade, radius, number;
+
+            number = 7;
+
+            if (number >= 5 && number <= 15)
+            {
+                Console.WriteLine("This code actually worked!");
+            }
+
+
+            //Console.WriteLine("Hey there! What is your current grade average (whole numbers) ?");
+            //Int32.TryParse(Console.ReadLine(), out grade);
+
+            //if (grade >= 80)
+            //{
+            //    Console.WriteLine("Good Job! That is a great grade average to have!");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Study harder!");
+            //}
+
+            //Console.WriteLine("Give me a radius");
+            //Int32.TryParse(Console.ReadLine(), out radius);
+            //if (radius <0)
+            //{
+            //    Console.WriteLine("Negative radii are illegal!");
+            //}
+            //else if (radius == 0)
+            //{
+            //    Console.WriteLine("You have a dot.");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("The diameter is " + radius*2 + ".");
+            //}
+            
         }
     }
 }
